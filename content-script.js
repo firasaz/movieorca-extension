41318c845de60bf56e376248ba5c8c47c4a1ddca
Firas Abuzahra <firasabuzahra@Firass-MacBook-Pro.local>
2024-10-26 21:59:00 -0700

    initial commit

---
+++ content-script.js
@@ -0,0 +1,9 @@
+const breadcrumb = document.getElementsByClassName("prebreadcrumb")[0]
+const alert = document.getElementsByClassName("alert")[0]
+
+if(breadcrumb) {
+    breadcrumb.style.display = 'none'
+}
+if(alert) {
+    alert.style.display = 'none'
+}
